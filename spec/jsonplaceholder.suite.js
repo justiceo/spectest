e4677512b7db428c5e037d28cfb437ef0340f0c7
Justice Ogbonna <justiceo@users.noreply.github.com>
2025-07-07 19:35:36 -0700

    Add a slightly more interesting case

--- spec/jsonplaceholder.suite.js
+++ spec/jsonplaceholder.suite.js
@@ -3,5 +3,29 @@ const suite = [
     name: "Fetch TODO 1",
     endpoint: "/todos/1",
   },
+  {
+    name: "Create a post",
+    endpoint: "/posts",
+    request: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: {
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      },
+    },
+    response: {
+      status: 201,
+      body: {
+        id: 101,
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      },
+    },
+  }
 ];
 export default suite;
